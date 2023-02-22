7323c897ec85a96a1592013eb10174d74344179d
@DiegoCascavita <72409256+DiegoCascavita@users.noreply.github.com>
2023-02-22 15:41:05 -0500

    V 2.0
    uploading mi homeworks in react.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./coe9c48b4bb10c9885a20151a/components/Navbar"
+import Main from "./coe9c48b4bb10c9885a20151a/components/Main"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="coe9c48b4bb10c9885a20151a/images/react-icon-smaller.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
